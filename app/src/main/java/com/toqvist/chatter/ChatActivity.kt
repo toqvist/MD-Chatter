f4d429e7abf4079dcdc296b1124e8b69cd46f57c
toqvist <tobias-ahlqvist@outlook.com>
2022-11-26 16:48:46 +0100

    Messages can be added to chat

--- app/src/main/java/com/toqvist/chatter/ChatActivity.kt
+++ app/src/main/java/com/toqvist/chatter/ChatActivity.kt
@@ -2,10 +2,37 @@ package com.toqvist.chatter
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+
 
 class ChatActivity : AppCompatActivity() {
+
+    var chatHistory: ChipGroup? = null
+    var chatMessages = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
+        chatHistory = findViewById<ChipGroup>(R.id.chatHistory)
+
+        addMessage("Hello")
+        addMessage("How are you?")
+        addMessage("Hello")
+        addMessage("How are you?")
+        addMessage("Hello")
+        addMessage("How are you?")
+
+    }
+
+
+    fun addMessage(message: String) {
+        chatMessages++
+
+        val newChip = Chip(this)
+        newChip.setText(message)
+
+        chatHistory?.addView(newChip)
     }
 }
